bd4eac967131c3bfea0df966496dcbe237038032
Darren Schnare <darren@darrenschnare.com>
2013-02-13 21:54:04 -0500

    Resolve bugs with Opera and FireFox

--- dschnare-js1k2013.js
+++ dschnare-js1k2013.js
@@ -2,20 +2,19 @@ d = document;
 n = b.addEventListener;
 l = d.createElement;
 h = b.appendChild;
-f = 'monospace';
-
+t = 'equestAnimationFrame';
+g = window;
+q = g['r' + t] || g['mozR' + t] || function(fn) { setTimeout(fn, 16.66); };
 
 b.style.textAlign = 'center';
 c.style.border = 'thin solid black';
 
 s = l.call(d, 'h1');
-s.style.fontFamily = f;
 s.innerHTML = 'DIGI-SKETCH';
 b.insertBefore(s, c);
 
 s = l.call(d, 'div');
-s.style.fontFamily = f;
-s.innerHTML = 'Z=left X=right &gt;=up ?=down';
+s.innerHTML = 'A=left S=right K=up L=down';
 h.call(b, s);
 
 m();
@@ -23,7 +22,7 @@ m();
 function m(w, kx, ky, v, px, py, s, bg, fg) {
   w = 600;
   c.width = c.height = w;
-  c.focus();
+  b.focus();
   a.fillStyle = bg = '#969696';
   a.fillRect(0,0,w,w);
   v = 1;
@@ -43,23 +42,23 @@ function m(w, kx, ky, v, px, py, s, bg, fg) {
 
   n.call(b, 'keydown', function (e, k) {
     k = e.keyCode;
-    kx = k==90 || k==88 ? k : kx;
-    ky = k==190 || k==191 ? k : ky;
+    kx = k==65 || k==83 ? k : kx;
+    ky = k==75 || k==76 ? k : ky;
   });
   n.call(b, 'keyup', function (e) {
     k = e.keyCode;
-    kx = k==90 || k==88 ? -1 : kx;
-    ky = k==190 || k==191 ? -1 : ky;
+    kx = k==65 || k==83 ? -1 : kx;
+    ky = k==75 || k==76 ? -1 : ky;
   });
 
   a.fillStyle = fg = '#1E1E1E';
   function update() {
-    if (kx==90) px -= v;
-    if (kx==88) px += v;
-    if (ky==190) py -= v;
-    if (ky==191) py += v;
+    if (kx==65) px -= v;
+    if (kx==83) px += v;
+    if (ky==75) py -= v;
+    if (ky==76) py += v;
     a.fillRect(px, py, 1, 1);
-    requestAnimationFrame(update);
+    q(update);
   }
   update();
 }
